c9e23a52d5a1c64a979ace56a605d80c8927818e
Chalermporn Posoppitakwong <Chalermporn.posoppitakwong@krungthai.com>
2025-01-14 23:27:31 +0700

    style: format article component and update compatibility date in nuxt config

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  compatibilityDate: '2025-01-14',
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
